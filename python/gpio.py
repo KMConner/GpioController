009db2c0af31c1e082c3d1b55f685e037708a04f
KMConner <KMConner@users.noreply.github.com>
2019-03-23 02:23:00 +0900

    Implemented GPIO.py
    Without lock

--- python/gpio.py
+++ python/gpio.py
@@ -1,14 +1,26 @@
-from typing import List
+from typing import Dict, List
+
+from gpiozero import LED
 
 from gpio_stub import GpioStub
 
 
 class Gpio(GpioStub):
     def __init__(self, gpio_in: List[int]):
-        pass
+        super().__init__(gpio_in)
+        self.led: Dict[int, LED] = {}
 
-    def set_gpio(self, gpio_number: int, state: bool):
-        raise NotImplementedError()
+        for gpio in gpio_in:
+            self.led[gpio] = LED(gpio)
+
+    def set_gpio(self, gpio_number: int, state: bool) -> None:
+        self._check_number(gpio_number)
+        self.led[gpio_number].value = 1 if state else 0
 
     def get_gpio(self, gpio_number: int) -> bool:
-        raise NotImplementedError()
+        self._check_number(gpio_number)
+        return False if self.led[gpio_number] == 0 else True
+
+    def _check_number(self, num: int):
+        if num not in self.led:
+            raise ValueError()
