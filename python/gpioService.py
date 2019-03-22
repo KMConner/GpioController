f011b1e5955e9cc32c10307be8f949b37b99d180
KMConner <KMConner@users.noreply.github.com>
2019-03-23 03:15:02 +0900

    Added log output from each command

--- python/gpioService.py
+++ python/gpioService.py
@@ -20,15 +20,16 @@ class GpioService(GpioServicer):
             self.gpio: GpioStub = Gpio(config.gpio_out)
 
     def GetGpio(self, request, context) -> GetGpioResponse:
-        logging.debug('[GetGpio - Begin]: GpioNumber: %d' % request.GpioNumber)
+        logging.debug('[GetGpio]: GpioNumber: %d' % request.GpioNumber)
         return GetGpioResponse(GpioNumber=request.GpioNumber,
                                State=gpio_pb2.On if self.gpio.get_gpio(request.GpioNumber) else gpio_pb2.Off)
 
     def SetGpio(self, request, context):
-        print(request)
+        logging.debug('[SetGpio]: GPIONumber: %d, GPIO State: %s' % (request.GpioNumber, request.State))
         self.gpio.set_gpio(request.GpioNumber, request.State == gpio_pb2.On)
         return SetGpioResponse()
 
     def BlinkGpio(self, request, context):
+        logging.debug('[BlinkGpio]: GPIONumber: %d, Length State: %f' % (request.GpioNumber, request.Length))
         self.gpio.blink_gpio(request.GpioNumber, request.Length)
         return BlinkGpioResponse()
